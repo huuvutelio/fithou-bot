571af890fa994264992f3dc4ab5ff34cf04134d6
nc-minh <nguyencongminh.official@gmail.com>
2022-11-01 10:50:53 +0700

    fix: uncomment

--- src/jobs/index.ts
+++ src/jobs/index.ts
@@ -7,16 +7,18 @@ rule.tz = 'Asia/Ho_Chi_Minh';
 
 rule.minute = config.jobs.fithou.second;
 
-// import { crawlFithouJob } from './crawlFithouJob';
-// import { examDaySchedule } from './examDay';
-// import { eveningSchedule, morningSchedule, noonSchedule } from './schoolSchedule';
+import { crawlFithouJob } from './crawlFithouJob';
+import { examDaySchedule } from './examDay';
+import { eveningSchedule, morningSchedule, noonSchedule } from './schoolSchedule';
 
 const runjobs = () => {
-  // schedule.scheduleJob(rule, crawlFithouJob);
-  // schedule.scheduleJob('00 45 6 * * 0-6', morningSchedule);
-  // schedule.scheduleJob('00 00 12 * * 0-6', noonSchedule);
-  // schedule.scheduleJob('00 30 16 * * 0-6', eveningSchedule);
-  // schedule.scheduleJob('00 00 20 * * 0-6', examDaySchedule);
+  schedule.scheduleJob(rule, crawlFithouJob);
+
+  schedule.scheduleJob('00 45 6 * * 0-6', morningSchedule);
+  schedule.scheduleJob('00 00 12 * * 0-6', noonSchedule);
+  schedule.scheduleJob('00 30 16 * * 0-6', eveningSchedule);
+
+  schedule.scheduleJob('00 00 20 * * 0-6', examDaySchedule);
 };
 
 export default runjobs;
