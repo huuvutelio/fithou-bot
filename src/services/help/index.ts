8262c8f08bbf960a9270a2b932dc2f494196464a
nc-minh <nguyencongminh.official@gmail.com>
2022-11-01 10:49:29 +0700

    feat: help featured

---
+++ src/services/help/index.ts
@@ -0,0 +1,8 @@
+export const help = () => {
+  return `*Các lệnh hỗ trợ:*
+/login: Đăng nhập CTMS
+/logout: Đăng xuất CTMS
+/help: Hiển thị hướng dẫn
+/examday: Đăng ký nhận thông báo ngày thi
+/un_examday: Hủy đăng ký nhận thông báo ngày thi`;
+};
