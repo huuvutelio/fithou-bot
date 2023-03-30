82cb3c6ac740cb2ddc139037039e80dca5e06556
nc-minh <nguyencongminh.official@gmail.com>
2023-03-30 13:25:27 +0700

    fix: [update github CI]

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,17 +1,17 @@
-import dotenvSafe from 'dotenv-safe';
+// import dotenvSafe from 'dotenv-safe';
 import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-import path from 'path';
-import fs from 'fs';
+// import path from 'path';
+// import fs from 'fs';
 
 dotenv.config();
-const pathEnv = path.join(__dirname, `../../.env.${process.env.NODE_ENV || 'dev'}`);
-if (fs.existsSync(pathEnv)) {
-  dotenvSafe.config({
-    allowEmptyValues: true,
-    path: pathEnv,
-    sample: path.join(__dirname, '../../.env.example'),
-  });
-}
+// const pathEnv = path.join(__dirname, `../../.env.${process.env.NODE_ENV || 'dev'}`);
+// if (fs.existsSync(pathEnv)) {
+//   dotenvSafe.config({
+//     allowEmptyValues: true,
+//     path: pathEnv,
+//     sample: path.join(__dirname, '../../.env.example'),
+//   });
+// }
 
 export default {
   env: process.env.NODE_ENV,
